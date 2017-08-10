adbd457fa6d435e1c570f249b3395280a61dfc0c
hirchaudhary <hir.chaudhary@gmail.com>
2017-08-10 11:47:02 -0400

    updated retroBarcode

--- AngularJS/retroBarcode/src/app/app.component.ts
+++ AngularJS/retroBarcode/src/app/app.component.ts
@@ -1,53 +1,21 @@
 import { Component } from '@angular/core';
 
-let rand = function(){
-  return Math.floor(Math.random()*255);
-}
-
-let randSize = function(){
-  return Math.floor(Math.random()*15);
-}
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 
-
 export class AppComponent {
     title = 'Retro Barcode Generator';
-  
-    colors = [
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-        {color:rand()+","+rand()+","+rand(), size:randSize()+"px"},
-    ]
+    colors = looping();
+}
+
+let looping = function(){
+  var colors = [];
+  for(let i = 0; i <50; i++){
+    var rand = {color:'#' + Math.floor(Math.random() * 16777215).toString(16), size:Math.floor(Math.random()*15)+"px"}
+    colors.push(rand);
+  }
+  return colors;
 }
